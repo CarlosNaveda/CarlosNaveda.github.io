aa9d2d5a0d1a65b2725eea4632f7bada0f123b8a
Carlos Naveda <navedacarlosm@gmail.com>
2026-05-15 20:53:35 -0500

    complete mapTitles and mapJobs

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,17 @@ const mapTitles = [
   {
     index: 1,
     title: 'Experiencia',
-    annotation: 'En cada lugar aprendí nuevas cosas valiosas hasta hoy.'
+    annotation: 'En cada lugar encontré algo nuevo que aprender.'
+  },
+  {
+    index: 2,
+    title: 'ToNextAxis',
+    annotation: 'Mi lugar para crear, pensar y documentar.'
+  },
+  {
+    index: 3,
+    title: 'Blog',
+    annotation: 'Porque compartir también es aprender.'
   }
 ];
 
@@ -35,7 +45,7 @@ const mapJobs = [
     logo: "/images/jobs/toNextAxis.png",
     company: "ToNextAxis",
     role: "Content Creator",
-    description: "Canal de YouTube donde documento explico lo aprendido sobre tecnología y sistemas de forma simple para hispanohablantes.",
+    description: "Canal de YouTube donde documento y explico lo aprendido sobre tecnología y sistemas de forma simple para hispanohablantes.",
     tags: ["YouTube", "Davinci Resolve", "StoryTelling", "Edición Narrativa", "Comunicación técnica"]
   },
   {
@@ -45,8 +55,68 @@ const mapJobs = [
     logo: "/images/jobs/canvia.jpg",
     company: "Canvia",
     role: "Quality Analyst III",
-    description: "Analista de calidad en empresa de tecnología financiera, responsable de diseñar y ejecutar pruebas para garantizar la calidad del software.",
-    tags: ["Quality Assurance","AS400","Web","Apps","WebSphere","SQL","Jira", "Confluence", "Metodologías Ágiles", "Jenkins","JFrog", "Bitbucket"]
+    description: "Analista de calidad en Digital Factory de Scotiabank Perú, asegurando la calidad de la plataforma de Banca por Internet Web y App.",
+    tags: ["Quality Assurance", "AS400", "Web", "Apps", "WebSphere", "SQL Server", "Jira", "Confluence", "Metodologías Ágiles", "Jenkins", "JFrog", "Bitbucket"]
+  },
+  {
+    index: 2,
+    start: "Feb 2019",
+    end: "Nov 2024",
+    logo: "/images/jobs/tata.jpg",
+    company: "Tata Consultancy Services",
+    role: "IT Analyst",
+    description: "Analista de calidad en Digital Factory de Scotiabank Perú, asegurando la calidad de la plataforma de Banca por Internet Web y App.",
+    tags: ["Quality Assurance", "AS400", "SQL Server", "DB2", "Jira", "Confluence", "Jenkins", "JFrog", "Bitbucket", "WebSphere", "Metodologías Ágiles"]
+  },
+  {
+    index: 3,
+    start: "Nov 2017",
+    end: "Feb 2019",
+    logo: "/images/jobs/ses.jpg",
+    company: "Software Enterprise Services",
+    role: "Quality Analyst",
+    description: "Analista de calidad en proyectos Digital Factory y Latam CREO de Scotiabank Perú, trabajando con metodologías ágiles y clásicas.",
+    tags: ["Quality Assurance", "AS400", "SQL Server", "DB2", "Jira", "Confluence", "Metodologías Ágiles"]
+  },
+  {
+    index: 4,
+    start: "Abr 2016",
+    end: "Sep 2017",
+    logo: "/images/jobs/choucair.jpg",
+    company: "Choucair Testing",
+    role: "Quality Analyst",
+    description: "Analista de pruebas funcionales en el proyecto Homebanking Personas de BanBif.",
+    tags: ["Quality Assurance", "Selenium WebDriver", "SQL", "iOS", "Android", "Team Foundation Server"]
+  },
+  {
+    index: 5,
+    start: "Feb 2014",
+    end: "Feb 2016",
+    logo: "/images/jobs/ibm.svg",
+    company: "IBM del Perú",
+    role: "Quality Analyst",
+    description: "Analista de calidad en Testing Factory para América Móvil Perú, liderando equipos y gestionando pruebas de caja blanca y negra.",
+    tags: ["Quality Assurance", "IBM RQM", "IBM RFT", "PL/SQL", "WebLogic", "SoapUI", "Unix"]
+  },
+  {
+    index: 6,
+    start: "Feb 2013",
+    end: "Dic 2013",
+    logo: "/images/jobs/delaware.jpg",
+    company: "Delaware Perú",
+    role: "Systems Consultant II",
+    description: "Consultor de sistemas en proyecto de Interbank, gestionando pruebas de caja negra y coordinación con stakeholders.",
+    tags: ["Quality Assurance", "CICS", "Mantis", "Spira Team", "SQL"]
+  },
+  {
+    index: 7,
+    start: "Sep 2011",
+    end: "Ene 2013",
+    logo: "/images/jobs/delaware.jpg",
+    company: "Delaware Perú",
+    role: "Systems Consultant I",
+    description: "Consultor de sistemas en proyecto de Telefónica del Perú, ejecutando pruebas de caja blanca en sistemas de facturación y cobros.",
+    tags: ["Quality Assurance", "PL/SQL", "Unix", "COBOL", "JCL", "OS/390"]
   }
 ];
 
@@ -111,7 +181,19 @@ export default function Home() {
       {/* Experiencia */}
       <section id={mapSections['Experiencia']} className="min-h-screen flex flex-col items-center justify-center gap-6 px-8 text-balanced">
         <Title title={mapTitles[1]} />
-        {mapJobs.map((job) => (<JobCard key={job.index} job={job} />))}     
+        {mapJobs.map((job) => (<JobCard key={job.index} job={job} />))}    
+        {buttonBounceGoTo(mapSections['ToNextAxis'])}  
+      </section>
+
+      {/* ToNextAxis */}
+      <section id={mapSections['Experiencia']} className="min-h-screen flex flex-col items-center justify-center gap-6 px-8 text-balanced">
+        <Title title={mapTitles[2]} />
+         {buttonBounceGoTo(mapSections['Blog'])}  
+      </section>
+
+      {/* Blog */}
+      <section id={mapSections['Blog']} className="min-h-screen flex flex-col items-center justify-center gap-6 px-8 text-balanced">
+        <Title title={mapTitles[3]} />           
       </section>
 
 
