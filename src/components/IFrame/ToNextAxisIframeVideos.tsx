bbc1eedfa669ae4dfc05aecdd714ad168c2c58c6
Carlos Naveda <navedacarlosm@gmail.com>
2026-05-18 12:48:34 -0500

    add modal videos to toNextAxis page

--- src/components/IFrame/ToNextAxisIframeVideos.tsx
+++ src/components/IFrame/ToNextAxisIframeVideos.tsx
@@ -1,9 +1,28 @@
 import IframeVideos from "./IframeVideos";
+import { useState } from "react";
 
 
 const ToNextAxisIframeVideos = ({video}: {video: IframeVideos}) => {
+    
+    const [videoActive, setVideoActive] = useState<string | null>(null);
+    
     return (
-        <iframe className="w-full max-w-3xl aspect-video" src={video.source} title={video.title}></iframe>                
+        <>  
+            {/* Últimos videos de ToNextAxis */}
+            <div onClick={() => setVideoActive(`${video.source}?autoplay=1&vq=hd1080`)}> 
+                <iframe className="toNextAxis-last-videos w-full max-w-3xl aspect-video" src={video.source} title={video.title}>
+                </iframe>                
+            </div>               
+
+            {/* Video seleccionado */}
+            {videoActive && (
+              <div className="toNextAxis-video-modal">
+                <div className="toNextAxis-video-overlay" onClick={() => setVideoActive(null)}/>
+                <iframe className="toNextAxis-video-selected w-full max-w-3xl aspect-video" src={videoActive} allow="autoplay" allowFullScreen>
+                </iframe>            
+             </div>       
+            )}            
+        </>
     )
 }
 
