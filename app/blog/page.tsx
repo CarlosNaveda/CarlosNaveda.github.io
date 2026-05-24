fd4a9685cf971c4aaf896914f57a40f2fa70e79d
Carlos Naveda <navedacarlosm@gmail.com>
2026-05-24 05:11:16 -0500

    feat: animate filter by tags in blog main

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,28 +4,61 @@ import mapPosts from '../../src/data/posts';
 import PostPreview from '@/src/components/Post/PostPreview';
 import FooterBar from '@/src/components/FooterBar/FooterBar';
 
+
+function generateDistance(postIndex:number, tagIndex:number) :string {
+    const radius = 50 + (postIndex * 20) + (tagIndex * 10); // Radio diferente por tag
+    return `${radius}`;
+}
+ 
+function generateDelay(postIndex:number, tagIndex:number) :string {        
+   const delay = (postIndex * 1.5) + (tagIndex * 0.5);
+    return `${delay}s`;
+}
+
+function generateDuration(postIndex: number, tagIndex: number): string {
+  const duration = 8 + (postIndex * 2) + (tagIndex * 3); // Duración diferente
+  return `${duration}s`;  
+}
+
+function getUniqueArray(array: string[]): string[] {
+  const uniqueArray: string[] = [];
+  array.forEach((element) => {
+    if (!uniqueArray.includes(element)) {
+      uniqueArray.push(element);
+    }
+  });
+  return uniqueArray;
+
+}
+
 export default function BlogMain() {
+  
+  const allUniqueTags = getUniqueArray(mapPosts.flatMap(post => post.tag));
+
   return (    
     <main>
         <div className="blog-main min-h-screen flex flex-col items-center justify-center gap-1 md:gap-4 px-2 md:px-8 pb-20 md:pb-0 text-balanced"> 
-            <h2 className='title-filter text-2xl md:text-4xl font-outfit'>Filtrar por tag: </h2>            
-            <div className='blog-tags flex flex-col items-center justify-center center gap-2 font-dm-sans'>                
-                {mapPosts.map((post) => ( //Recorro el array de posts               
-                 <small key={post.index} className='flex flex-row items-center justify-center center gap-2'> 
-                    {post.tag.map((tag) => ( //Recorro el array de tags
-                        <span key={tag} className='tag-to-filter'>{tag}</span>
-                    ))
-                    }
-                 </small>                    
-                 ))
-                }
-            </div><hr /> 
+            <div className="blog-tags flex flex-col items-center justify-center center gap-2 font-dm-sans">
+                <h2 className='title-filter text-2xl md:text-4xl font-outfit'>Filtrar por tag</h2>             
+                <div className='wrapper-tags w-[300px] h-[200px] xs:w-[400px] xs:h-[250px] md:w-[500px] md:h-[300px] lg:w-[900px] lg:h-[500px]'>                                    
+                    <small className='flex flex-row items-center justify-center center gap-2'> 
+                        {allUniqueTags.map((tag, tagIndex) => ( //Recorro el array de tags únicos
+                            <span key={tag} className="tag-to-filter top-[55%] md:top-[50%]"  
+                            style={{'--orbit-radius': `${generateDistance(0, tagIndex)}px`, 
+                                animationDelay: `${generateDelay(0, tagIndex)}s`,
+                                animationDuration: generateDuration(0, tagIndex),  
+                            }as React.CSSProperties}>{tag}</span>  
+                        ))
+                        }
+                    </small>                    
+                </div>
+            </div>
             <h2 className='title-posts text-2xl md:text-4xl font-outfit'>Posts</h2>
             <div className='blog-main-posts-preview flex flex-col md:pb-20 lg:pb-auto xl:grid xl:grid-cols-2 items-center justify-center gap-2 md:gap-4'> 
                 <PostPreview mapPosts={mapPosts}/>
             </div>            
         </div>            
-       <FooterBar />
+       <FooterBar /> 
     </main>     
   );
 }
--- src/components/Post/PostPreview.tsx
+++ src/components/Post/PostPreview.tsx
@@ -18,13 +18,13 @@ const PostPreview = ({mapPosts}: {mapPosts: PostType[]}) =>{
                                     <h3 className="date text-xs md:text-base">{formatDateToString(mapPost.publishDate)}</h3> 
                                     <hr className="separator"></hr>
                                     <h3 className="text-xs md:text-base flex flex-row gap-2">
-                                        {mapPost.tag.map((tag) => ( //Recorro el array de tags
-                                            <span key={tag} className="tag">{tag}</span> 
+                                        {mapPost.tag.map((tag,tagIndex) => ( //Recorro el array de tags
+                                            <span key={tagIndex} className="tag">{tag}</span>  
                                         ))}
                                     </h3>
                                 </div>
                                 <h2 className="post-preview-title text-lg md:text-2xl font-outfit">{mapPost.title}</h2> 
-                                <p className="post-preview-shortDescription text-xs md:text-base">{mapPost.shortDescription}</p>     
+                                <p className="post-preview-shortDescription text-xs md:text-base">{mapPost.shortDescription}</p>      
                                 <Link className="post-slug-link text-sm md:text-lg lg:w-[90px]" href={`/blog/${formatTitleToKebabCase(mapPost.title)}`}>Leer más...</Link>
                             </div>
                             <Image src={mapPost.imageSource} alt={mapPost.title} className="post-preview-image object-cover w-[50] h-[50] md:w-[100] md:h-[100]" width={image_width} height={image_height}/>
