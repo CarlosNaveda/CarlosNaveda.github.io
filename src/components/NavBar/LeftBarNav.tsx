b88879e311852f193384872ee4e18938dc060d57
Carlos Naveda <navedacarlosm@gmail.com>
2026-05-25 00:30:22 -0500

    feat: add switch icons for accordion in mobilet, tablet, and refactor code leftbar

--- src/components/NavBar/LeftBarNav.tsx
+++ src/components/NavBar/LeftBarNav.tsx
@@ -3,7 +3,7 @@
 import {HouseHeart, UserStar, BriefcaseBusiness, CirclePlay, NotebookPen} from 'lucide-react';
 import {useSection} from '../../Hook/useSection';
 import { useState,useEffect,useRef } from 'react';
-import { ListCollapse } from 'lucide-react';
+import { ListPlus,ListMinus } from 'lucide-react';
 import Link from 'next/link'
 
 
@@ -12,25 +12,40 @@ export default function LeftBarNav() {
     
     const icon_size = 20;   
     const { activeSection } = useSection();    
+
+    //Items del LeftBar
+    const navItems = [
+        { id: 'inicio', label: 'INICIO', icon: <HouseHeart size={icon_size} /> },
+        { id: 'sobreMi', label: 'SOBRE MÍ', icon: <UserStar size={icon_size} /> },
+        { id: 'experiencia', label: 'EXPERIENCIA', icon: <BriefcaseBusiness size={icon_size} /> },
+        { id: 'tonextaxis', label: 'TONEXTAXIS', icon: <CirclePlay size={icon_size} /> },
+        { id: 'blog', label: 'BLOG', icon: <NotebookPen size={icon_size} /> },
+    ];
     
     //Para manejar la visibilidad del modo acordión
     const [currentState, setCurrentState] = useState(false);
+
+    //Para manejar el acordeón
     const accordionRef = useRef<HTMLDivElement | null>(null);
     
     useEffect(() => {
-        const handleClickOutside = (event: Event) => {            
-            // Si el acordeón está abierto y el clic fue fuera del elemento referenciado
-            if (accordionRef.current && event.target instanceof Node && !accordionRef.current.contains(event.target)) { 
-                setCurrentState(false);
-            }
-        };
-        
-        document.addEventListener('click', handleClickOutside);
-        document.addEventListener('touchend', handleClickOutside);        
+        const handleClickOutside = (event: Event) => {      
+            
+             const path = event.composedPath(); //Utilizado para evitar que el browser los detecte como si hubiera hecho click fuera del acordeón
+             const clickedInsideAccordion = path.some(
+                (el) => el instanceof HTMLElement && el.classList.contains('accordion')
+            );            
 
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-            document.removeEventListener('touchend', handleClickOutside);        
+            // Si el acordeón está abierto y el clic fue fuera del elemento referenciado
+            if (!clickedInsideAccordion) {                 
+                setCurrentState(false);
+            }          
+        };        
+        
+        document.addEventListener('click', handleClickOutside,);                 
+
+        return () => { 
+            document.removeEventListener('click', handleClickOutside);             
         };
     }, []);
     
@@ -105,79 +120,36 @@ export default function LeftBarNav() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
     }, []);    
+
+    const isMobileTablet = !isDesktop;
+    const classConfigDesktop = `left-bar flex flex-col transition-all duration-500 ${animationClass} left-[1%] top-[6%] xs:top-[5%] md:top-[4%] lg:top-[40%] text-xs md:text-xl`;
+    const backgroundMobileTablet = "max-lg:bg-[rgba(18,17,43,0.85)] max-lg:backdrop-blur-md max-lg:border max-lg:border-[rgba(126,122,222,0.2)] max-lg:rounded-xl p-3";     
+    const classConfigMobileTablet = `left-bar flex flex-col transition-all duration-500 left-[1%] top-[6%] xs:top-[5%] md:top-[4%] lg:top-[40%] ${backgroundMobileTablet} text-xs md:text-base ${currentState ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2 pointer-events-none"}`
     
-    function navLeftBarDesktop() { 
-        return (         
-        <aside className={`left-bar flex flex-col transition-all duration-500 ${animationClass} left-[1%] top-[6%] xs:top-[5%] md:top-[4%] lg:top-[40%] text-xs md:text-xl`}>      
-            <nav className='font-outfit'>
-                <ul className='left-bar-nav flex flex-col items-left gap-2'> 
-                    <li className={activeSection === "inicio" ? "active-left-nav" : "inactive-left-nav"}>
-                        <HouseHeart size={icon_size}/>
-                        <Link className='transition-all' href={"#inicio"} onClick={() => setAnimationClass("opacity-0 pointer-events-none")}>INICIO</Link>                        
-                    </li>
-                    <li className={activeSection === "sobreMi" ? "active-left-nav" : "inactive-left-nav"}>
-                        <UserStar size={icon_size}/>
-                        <Link href={"#sobreMi"}>SOBRE MÍ</Link>                        
-                    </li>
-                    <li className={activeSection === "experiencia" ? "active-left-nav" : "inactive-left-nav"}>
-                        <BriefcaseBusiness size={icon_size}/>
-                        <Link href={"#experiencia"}>EXPERIENCIA</Link>                        
-                    </li>                    
-                    <li className={activeSection === "tonextaxis" ? "active-left-nav" : "inactive-left-nav"}> 
-                        <CirclePlay size={icon_size}/>
-                        <Link href={"#tonextaxis"}>TONEXTAXIS</Link>                        
-                    </li>
-                    <li className={activeSection === "blog" ? "active-left-nav" : "inactive-left-nav"}>
-                        <NotebookPen size={icon_size}/>
-                        <Link href={"#blog"}>BLOG</Link>                        
-                    </li>                    
-                </ul>                                       
-            </nav>          
-        </aside>            
-        );
-    }
-
-
-    function navLeftBarMobileTablet() { 
-        
-        const backgroundMobileTablet = "max-lg:bg-[rgba(18,17,43,0.85)] max-lg:backdrop-blur-md max-lg:border max-lg:border-[rgba(126,122,222,0.2)] max-lg:rounded-xl p-3";
-
-        return (
-            <div ref={accordionRef} className="accordion">     
-            <ListCollapse className="accordion-icon relative" onClick={() => setCurrentState(!currentState)}/>             
-                <aside className={`left-bar flex flex-col transition-all duration-500 left-[1%] top-[6%] xs:top-[5%] md:top-[4%] lg:top-[40%] ${backgroundMobileTablet} text-xs md:text-base ${currentState ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2 pointer-events-none"}`}>                               
-                    <nav>
+    function getAccordion()
+    {   return (                
+                currentState 
+                    ? (<ListMinus className="accordion-icon relative" onClick={() => {setCurrentState(!currentState)}}/>) 
+                    : (<ListPlus className="accordion-icon relative" onClick={() => {setCurrentState(!currentState)}}/>) 
+         );
+    }    
+ 
+    return (      
+           <div ref={accordionRef} className="accordion">
+                {isMobileTablet ? getAccordion() : null}
+                <aside className={isMobileTablet ? classConfigMobileTablet : classConfigDesktop}>                               
+                    <nav className='font-outfit'>
                         <ul className='left-bar-nav flex flex-col items-left gap-2'> 
-                            <li className={activeSection === "inicio" ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { e.stopPropagation(); setCurrentState(false); }}>
-                                 <HouseHeart size={icon_size}/>
-                                <Link className='transition-all' href={"#inicio"} >INICIO</Link>                        
-                            </li>
-                            <li className={activeSection === "sobreMi" ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { e.stopPropagation(); setCurrentState(false); }}>
-                                 <UserStar size={icon_size}/>
-                                <Link href={"#sobreMi"}>SOBRE MÍ</Link>    
-                            </li>
-                            <li className={activeSection === "experiencia" ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { e.stopPropagation(); setCurrentState(false); }}>
-                                <BriefcaseBusiness size={icon_size}/>
-                                <Link href={"#experiencia"}>EXPERIENCIA</Link>
-                            </li>                    
-                            <li className={activeSection === "tonextaxis" ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { e.stopPropagation(); setCurrentState(false); }}> 
-                                <CirclePlay size={icon_size}/>
-                                <Link href={"#tonextaxis"}>TONEXTAXIS</Link> 
-                            </li>
-                            <li className={activeSection === "blog" ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { e.stopPropagation(); setCurrentState(false); }}>
-                                <NotebookPen size={icon_size}/>
-                                <Link href={"#blog"}>BLOG</Link>
-                            </li>                    
+                            {navItems.map((item) => (
+                                <li key={item.id} className={activeSection === item.id ? "active-left-nav" : "inactive-left-nav"} onClick={(e) => { if(isMobileTablet) e.stopPropagation(); setCurrentState(false); }}>
+                                    {item.icon}
+                                    <Link className={`${item.id === "inicio" ? "transition-all" : ""}`} href={`#${item.id}`} onClick={() => { if (isMobileTablet) if (item.id === "inicio") setAnimationClass("opacity-0 pointer-events-none");}}>{item.label}</Link>                        
+                                </li>
+                            ))}                                               
                         </ul>                                       
                      </nav>       
                 </aside>                         
-        </div>    
-        );
-    } 
-    
- 
-    return (      
-        isDesktop ? navLeftBarDesktop() : navLeftBarMobileTablet()     
+           </div>  
     );
 }
 
