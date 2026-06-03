541b04ecbbb3e44900863d0c4398a49dd422aa82
Carlos Naveda <navedacarlosm@gmail.com>
2026-06-03 03:55:14 -0500

    fix: href empty and css config without use

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -45,23 +45,23 @@ const Accordion = () => {
                 <nav className="accordion-options w-auto h-auto bg-[var(--white)] absolute left-[5px] top-[25px] z-10">
                     <ul className='top-bar-nav flex flex-col'>  
                         <li className={`${mapSections['inicio']} text-xs flex flex-row items-center justify-left gap-1`} onClick={() => setCurrentState(false)}> 
-                            <a href=""><HouseHeart size={icon_size}/>
+                            <a href="#"><HouseHeart size={icon_size}/>
                             </a>{mapSections['inicio'].toUpperCase()}    
                         </li>
                         <li className={`${mapSections['sobreMi']} text-xs flex flex-row items-center justify-left gap-1`} onClick={() => setCurrentState(false)}> 
-                            <a href=""><UserStar size={icon_size}/>
+                            <a href="#"><UserStar size={icon_size}/>
                             </a>{mapSections['sobreMi'].toUpperCase()}  
                         </li>
                         <li className={`${mapSections['experiencia']} text-xs flex flex-row items-center justify-left gap-1`} onClick={() => setCurrentState(false)}> 
-                            <a href=""><BriefcaseBusiness size={icon_size}/>
+                            <a href="#"><BriefcaseBusiness size={icon_size}/>
                             </a>{mapSections['experiencia'].toUpperCase()}  
                         </li>                    
                         <li className={`${mapSections['tonextaxis']} text-xs flex flex-row items-center justify-left gap-1`} onClick={() => setCurrentState(false)}> 
-                            <a href=""><CirclePlay size={icon_size}/>
+                            <a href="#"><CirclePlay size={icon_size}/>
                             </a>{mapSections['tonextaxis'].toUpperCase()}  
                         </li>
                         <li className={`${mapSections['blog']} text-xs flex flex-row items-center justify-left gap-1`} onClick={() => setCurrentState(false)}> 
-                            <a href=""><NotebookPen size={icon_size}/>
+                            <a href="#"><NotebookPen size={icon_size}/>
                             </a>{mapSections['blog'].toUpperCase()}  
                         </li>                    
                     </ul>
